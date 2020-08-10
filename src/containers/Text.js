b6007e743b0e78515fbade7788aadec67cbc63d8
Evelinne Rodrigues <48141118+eesr05@users.noreply.github.com>
2020-08-10 18:00:24 -0300

    Corrigindo o texto

--- src/containers/Text.js
+++ src/containers/Text.js
@@ -11,8 +11,8 @@ function Text(params) {
          esse gerador de citações mostra diversas frases que podem ser usadas como parte de sua argumentação,
           aproveite. <br /><br />
 
-        As redações nota mil, em sua maioria, possuem ao menos duas citações, sejam de figuras públicas, não 
-        só filósofos, cientistas, e autores , como também alusões históricas do mundo em que vivemos, então
+        As redações nota mil, em sua maioria, possuem ao menos duas citações, sejam de figuras públicas, 
+       filósofos, cientistas, e autores , como também alusões históricas do mundo em que vivemos, então
          coloque em sua redação, porém tome cuidado em escolher pois tem que estar de acordo com o tema que
           está sendo desenvolvido. <br /><br />
         O que é preciso para redação perfeita:
@@ -21,17 +21,17 @@ function Text(params) {
          argumentos;  Apresentar uma proposta de intervenção para o problema. <br /><br/>
 
         Na introdução, deve-se apresentar e mostrar seu posicionamento sobre o tema, e antes de finalizar o 
-        paragrafo deve constar sua tese, que nada mais é que sua linha de raciocínio. Nos paragrafos de
-        desenvolvimento, voce deve defender sua tese, para sustentar sua argumentação utilize alusões historicas,
-         citações, filmes, entre outros, que estejam relacionados ao tema, e que tenha conexao com sua argumentação, 
+       parágrafo deve constar sua tese, que nada mais é que sua linha de raciocínio. Nos parágrafos de
+        desenvolvimento, você deve defender sua tese, para sustentar sua argumentação utilize alusões históricas,
+         citações, filmes, entre outros, que estejam relacionados ao tema, e que tenha conexão com sua argumentação, 
          por isso coloque tudo de uma forma mais clara e objetiva possível, para que fique uma redação organizada 
-         procure deixar cada paragrafo para um argumento especifico.  E nao se esqueça de usar conectivos, tais como,
+         procure deixar cada parágrafo para um argumento especifico.  E não se esqueça de usar conectivos, tais como,
           "não só… mas também", "ainda por cima", "A fim de', entre outros. <br /> <br/>
 
-        Reserve o ultimo paragrafo para a conclusão, na sua proposta de intervenção deve conter 5 elementos, sao eles,
+        Reserve o último parágrafo para a conclusão, na sua proposta de intervenção deve conter 5 elementos, são eles,
          o AGENTE, que deve fazer, o MODO, como deve ser feito, o MEIO como vai ser realizado, DETALHAMENTO, especificar
           com mais detalhes, e a FINALIDADE, o que se espera conseguir com sua proposta. O ideal é que seja retomado a 
-          citação feita no primeiro paragrafo, para que assim fique uma redação ciclica.<br /><br />
+          citação feita no primeiro parágrafo, para que assim fique uma redação cíclica.<br /><br />
       </p>
         </div>
         </div>
